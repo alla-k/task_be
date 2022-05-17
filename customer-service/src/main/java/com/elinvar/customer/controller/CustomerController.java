a6bea1264876bd0cf4ca12b451478c62889dd505
Alla Kriukova <alla.kriukova@elinvar.de>
2022-05-17 10:04:55 +0200

    Add customer API endpoint

---
+++ customer-service/src/main/java/com/elinvar/customer/controller/CustomerController.java
@@ -0,0 +1,38 @@
+package com.elinvar.customer.controller;
+
+import java.math.BigDecimal;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.server.ResponseStatusException;
+import com.elinvar.customer.service.OnboardCustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Slf4j
+@RestController
+public class CustomerController {
+
+    @Autowired
+    OnboardCustomerService onboardCustomerService;
+
+    @PostMapping("/addCustomer")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<String> addCustomer(@RequestParam("name") String name, @RequestParam("balance") BigDecimal balance)
+    {
+        try{
+            return ResponseEntity.ok(onboardCustomerService.onboardCustomer(UUID.randomUUID().toString(), name, balance));
+        }
+        catch (Exception ex){
+            log.error(ex.getMessage());
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Creating customer has failed.");
+        }
+
+
+    }
+}
