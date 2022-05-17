b1522b24173623843d0dcfa4d0fc992b5af54ac4
Alla Kriukova <alla.kriukova@elinvar.de>
2022-05-17 09:50:43 +0200

    Add customer API endpoint

--- customer-service/src/main/java/com/elinvar/customer/controller/CustomerController.java
+++ customer-service/src/main/java/com/elinvar/customer/controller/CustomerController.java
@@ -1,13 +1,12 @@
-package com.elinvar.pricehandler.controller;
+package com.elinvar.customer.controller;
 
 import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.util.Random;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.server.ResponseStatusException;
@@ -21,15 +20,16 @@ public class CustomerController {
     @Autowired
     OnboardCustomerService onboardCustomerService;
 
-    @GetMapping("/addCustomer")
+    @PostMapping("/addCustomer")
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<String> addCustomer(@RequestParam("name") String name, @RequestParam("balance") BigDecimal balance)
     {
         try{
-        return ResponseEntity.ok(onboardCustomerService.onboardCustomer(UUID.randomUUID().toString(), name, balance));
+            return ResponseEntity.ok(onboardCustomerService.onboardCustomer(UUID.randomUUID().toString(), name, balance));
         }
         catch (Exception ex){
             log.error(ex.getMessage());
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Creating cclient has failed.");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Creating customer has failed.");
         }
 
 
